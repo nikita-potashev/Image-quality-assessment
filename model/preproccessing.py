7d1382ccec2b74355920f753724b00d985ed86bf
dzuba <chepik29rus@gmail.com>
2019-03-16 22:14:23 +0300

    preproc, move util

--- model/preproccessing.py
+++ model/preproccessing.py
@@ -1,29 +1,60 @@
-import zipfile
-import shutil
 import glob
-import os
+import numpy as np
+from keras.utils import np_utils
+from keras.preprocessing import image
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+input_shape = (500, 500, 1)
 
 
-def parse_zip(path):
+def read_img(path):
     """[summary]
-
+    
     Arguments:
-        path [string] -- path to parse all zip files in folder
-
+        path {[type]} -- [description]
+    
     Returns:
-        [list] -- list of paths to all zips
+        [type] -- [description]
     """
-    pass
+    input_img = image.load_img(
+        path, target_size=input_shape, color_mode="grayscale")
+    input_img = image.img_to_array(input_img)
+    return input_img
 
-def extract_zip(path):
+
+def load_img(lst):
     """[summary]
-
+    
     Arguments:
-        path [string] -- extract from zip
-
+        lst {[type]} -- [description]
+    
     Returns:
-        [list] -- ??
+        [type] -- [description]
     """
+    arr = []
+    for img_path in lst:
+        arr.append(read_img(img_path))
+    temp = np.array(arr, np.float32)
+    return temp/255.0
 
-if __name__ == "__main__":
-    print(os.getcwd())
+
+raw_blur = glob.glob('models/Blur/data/train/blurry*.jpg')
+raw_clear = glob.glob('models/Blur/data/train/clear*.jpg')
+
+labels_blur = np.ones(len(raw_blur)).astype('int')
+labels_clear = np.zeros(len(raw_clear)).astype('int')
+
+raw_data = raw_blur + raw_clear
+labels = np.concatenate((labels_blur, labels_clear), axis=0)
+
+Y = np_utils.to_categorical(labels, 2)
+
+x, y = shuffle(raw_data, Y, random_state=2)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.25, random_state=2)
+
+X_train = load_img(X_train)
+X_test = load_img(X_test)
---
+++ model/util.py
@@ -0,0 +1,32 @@
+import zipfile
+import shutil
+import glob
+import os
+
+
+def parse_zip(path):
+    """[summary]
+
+    Arguments:
+        path [string] -- path to parse all zip files in folder
+
+    Returns:
+        [list] -- list of paths to all zips
+    """
+    pass
+
+
+def extract_zip(path):
+    """[summary]
+
+    Arguments:
+        path [string] -- extract from zip
+
+    Returns:
+        [list] -- ??
+    """
+    pass
+
+
+if __name__ == "__main__":
+    print(os.system("pwd"))
