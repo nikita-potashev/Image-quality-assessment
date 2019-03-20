aae3afd1b56393ab8502950bafcbd53f9cd14af8
dzuba <chepik29rus@gmail.com>
2019-03-20 23:21:13 +0300

    fix slurm script

--- model/run.sh
+++ model/run.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1   
 #SBATCH -c 20
 #SBATCH --mem=62000
-#SBATCH -o models/logs/%j_outfile
-#SBATCH -e models/logs/%j_errfile
+#SBATCH -o model/logs/%j_outfile
+#SBATCH -e model/logs/%j_errfile
 
 python3 train.py --model=Blur --input_shape 250 250 1 --num_epochs=10 --batch_size=32
