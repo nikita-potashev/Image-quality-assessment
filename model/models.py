92a706a1300c9df0bbd9f39172099fc3d9f5d2c5
dzuba <chepik29rus@gmail.com>
2019-03-22 20:15:09 +0300

    model reduce

--- model/models.py
+++ model/models.py
@@ -13,18 +13,15 @@ def blur_model(input_shape):
         [model] -- simple cnn model
     """
     model = Sequential()
-    model.add(Conv2D(32, (7, 7), activation='relu', input_shape=input_shape))
+    model.add(Conv2D(16, (5, 5), activation='relu', input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
-    model.add(Conv2D(64, (5, 5), activation='relu'))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
 
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-
     model.add(Dropout(0.2))
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(32, activation='relu'))
 
     model.add(Dropout(0.2))
     model.add(Dense(2, activation='sigmoid'))
