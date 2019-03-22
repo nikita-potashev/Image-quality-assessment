af82cdb474f059229afd1ada572b188b95bfdf81
dzuba <chepik29rus@gmail.com>
2019-03-22 20:34:48 +0300

    model blur fix [1]

--- model/models.py
+++ model/models.py
@@ -13,10 +13,10 @@ def blur_model(input_shape):
         [model] -- simple cnn model
     """
     model = Sequential()
-    model.add(Conv2D(16, (5, 5), activation='relu', input_shape=input_shape))
+    model.add(Conv2D(32, (5, 5), activation='relu', input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
-    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
 
