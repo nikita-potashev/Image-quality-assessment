61b15a895e5eca19165cefea0e18d530692448b6
dzuba <chepik29rus@gmail.com>
2019-03-22 22:47:02 +0300

    redusce model blur[1]

--- model/models.py
+++ model/models.py
@@ -13,18 +13,19 @@ def blur_model(input_shape):
         [model] -- simple cnn model
     """
     model = Sequential()
-    model.add(Conv2D(32, (5, 5), activation='relu', input_shape=input_shape))
+    model.add(Conv2D(8, (5, 5), activation='relu', input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(16, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
 
     model.add(Flatten())
-    model.add(Dropout(0.2))
     model.add(Dense(32, activation='relu'))
-
     model.add(Dropout(0.2))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dropout(0.3))
+
     model.add(Dense(2, activation='sigmoid'))
 
     model.compile(loss='binary_crossentropy',
