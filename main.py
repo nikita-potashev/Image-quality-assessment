0af6acf8141c11bd79dc84c377c12ba7f011d3b5
dzuba <chepik29rus@gmail.com>
2019-03-24 02:16:56 +0300

    small prediction

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import glob
 from model import preproc
 
 if __name__ == "__main__":
-    json_file = open("model/json/blur.json", "r")
+    json_file = open("model/json/arch/blur.json", "r")
     loaded_model_json = json_file.read()
     json_file.close()
-    # Создаем модель на основе загруженных данных
+
     model = model_from_json(loaded_model_json)
-    # Загружаем веса в модель
+
     model.load_weights("model/weights/blur.h5")
 
-    data = glob.glob('/home/nick/Desktop/databrelease2/gblur/*.bmp')
+    data = glob.glob('/home/nick/Desktop/predict/blur/*')
+
+    blurred = preproc.load_imgs(data,(250,250,1),'grayscale')
+
+
+    score = model.predict_classes(blurred)
+    print(score)
