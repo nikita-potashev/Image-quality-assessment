d5fd5d8023ce60c6a2a2d869516592652f45e82c
dzuba <chepik29rus@gmail.com>
2019-03-23 22:42:36 +0300

    load blur model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-from keras.models import load_model
+from keras.models import model_from_json
 
 
 
 if __name__ == "__main__":
-    pass
+    json_file = open("model/json/blur.json", "r")
+    loaded_model_json = json_file.read()
+    json_file.close()
+    # Создаем модель на основе загруженных данных
+    model = model_from_json(loaded_model_json)
+    # Загружаем веса в модель
+    model.load_weights("model/weights/blur.h5")
