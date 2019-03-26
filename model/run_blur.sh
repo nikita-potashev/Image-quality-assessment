a3b46c3055540ebd9e765fd02992b984cffa6d93
dzuba <chepik29rus@gmail.com>
2019-03-27 00:36:27 +0300

    slurm scripts for different models

--- model/run_blur.sh
+++ model/run_blur.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1   
 #SBATCH -c 20
 #SBATCH --mem=62000
-#SBATCH -o logs/%j_outfile
-#SBATCH -e logs/%j_errfile
+#SBATCH -o logs/%j_blurout
+#SBATCH -e logs/%j_blurerr
 
 python3 train.py --model=Blur --input_shape 500 500 1 --num_epochs=10 --batch_size=32 --train_mode=cluster
---
+++ model/run_noise.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1   
+#SBATCH -c 20
+#SBATCH --mem=62000
+#SBATCH -o logs/%j__noiseout
+#SBATCH -e logs/%j_noiseerr
+
+python3 train.py --model=Noise --input_shape 500 500 1 --num_epochs=10 --batch_size=32 --train_mode=cluster
