997d80467ab9201ad741033d517bdb430ffda692
Nick <chepik29rus@gmail.com>
2019-04-10 01:46:31 +0300

    append err file out to slurm scripts

--- run_blur.sh
+++ run_blur.sh
@@ -3,5 +3,6 @@
 #SBATCH -n 1   
 #SBATCH -c 20
 #SBATCH --mem=62000
-#SBATCH --output=slurm/%j_out
+#SBATCH --output=slurm/
+#SBATCH --error=slurm/
 python3 main_blur.py --c configs/blur_model_cluster.json
