ff44ad751ffb8f9155f5e9dedbf0bf34dd41b930
Nick <chepik29rus@gmail.com>
2019-04-14 01:46:21 +0300

    extend noise slurm script

--- run_noise.sh
+++ run_noise.sh
@@ -3,6 +3,8 @@
 #SBATCH -n 1   
 #SBATCH -c 20
 #SBATCH --mem=62000
-#SBATCH --output=slurm/
-#SBATCH --error=slurm/
-python3 main_noise.py --c configs/noise_model_cluster.json
+#SBATCH --output=slurm/%j_out
+#SBATCH --error=slurm/%j_err
+python3 main_noise.py --c configs/noise/noise_model1.json
+python3 main_noise.py --c configs/noise/noise_model2.json
+python3 main_noise.py --c configs/noise/noise_model3.json
