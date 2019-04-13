9aaf9a10ff56f5feb6c9d7ba1e292ba9e1ca0552
Nick <chepik29rus@gmail.com>
2019-04-14 00:51:58 +0300

    out slurm

--- run_blur.sh
+++ run_blur.sh
@@ -3,8 +3,8 @@
 #SBATCH -n 1   
 #SBATCH -c 20
 #SBATCH --mem=62000
-#SBATCH --output=slurm/
-#SBATCH --error=slurm/
+#SBATCH --output=slurm/%j_out
+#SBATCH --error=slurm/%j_err
 python3 main_blur.py --c configs/blur/blur_model1.json
 python3 main_blur.py --c configs/blur/blur_model2.json
 python3 main_blur.py --c configs/blur/blur_model3.json
